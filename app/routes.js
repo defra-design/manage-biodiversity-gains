236b1753d5bbab221c3af60de95f5965d66e5b75
Alex Morris <alex@thedoidea.co>
2024-01-24 09:25:27 +0000

    Removed debug routing info

--- app/routes.js
+++ app/routes.js
@@ -14,22 +14,3 @@ router.use(radioButtonRedirect)
 const registerapplicationv22 = require('./views/register-application/v22/_routes');
 router.use('/register-application/v22', registerapplicationv22);
 
-// Adding "query" to every page. There could be a better implementaion using middleware. 
-// in nunjkucks you can call {{query[key]}}
-router.get('*', function(req, res) {
-  let path = req.params[0]
-  let pathEnd = path.slice(-1)
-  let newPath = path.substring(1)
-  if (path == "/") {
-	// Top home page
-	path = "index"
-  } else if (pathEnd == "/") {
-	// Check if this is the route folder, if so, then render the index page.
-	path = newPath + "index"
-  } else {
-	path = newPath
-  }
-  res.render(path, {
-	"query": req.query,
-  });
-})
