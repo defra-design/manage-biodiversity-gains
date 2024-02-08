27510b402e5bddf5a97ad521b5018c6e51f34ae7
Karl Parton <karlparton@gmail.com>
2024-02-08 17:22:53 +0000

    Credits pay. Public reg start.

--- app/views/credits-buy/v4/_routes.js
+++ app/views/credits-buy/v4/_routes.js
@@ -6,11 +6,24 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes above the module.exports line
 
-// Purchase order
+// Credits tier
+router.post('/credit-tiers', function (req, res) {
+    res.redirect('cost');
+});
 
+// Credits cost
+router.post('/cost', function (req, res) {
+    res.redirect('tasklist');
+});
+
+// Purchase order
 router.post('/purchase-order', function (req, res) {
     res.redirect('tasklist');
 });
 
+// Terms and conditions
+router.post('/terms-and-conditions', function (req, res) {
+    res.redirect('tasklist');
+});
 
 module.exports = router
