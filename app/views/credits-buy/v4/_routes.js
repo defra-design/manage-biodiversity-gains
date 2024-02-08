380f6a8a1580798306e707dad703861211ebb400
Karl Parton <karlparton@gmail.com>
2024-02-08 15:48:26 +0000

    Copy updates.

--- app/views/credits-buy/v4/_routes.js
+++ app/views/credits-buy/v4/_routes.js
@@ -6,5 +6,11 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes above the module.exports line
 
+// Purchase order
+
+router.post('/purchase-order', function (req, res) {
+    res.redirect('tasklist');
+});
+
 
 module.exports = router
